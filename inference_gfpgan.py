d0e592c51b7aee6fdf642f934d931284f19c75cf
Yuri Edward <yuri6037@outlook.com>
2021-12-02 14:26:05 +0100

    Added skip for bpx files

--- inference_gfpgan.py
+++ inference_gfpgan.py
@@ -69,11 +69,14 @@ def main():
         for img_path in img_list:
             # read image
             img_name = os.path.basename(img_path)
-            print(f'Processing {img_name} ...')
             basename, ext = os.path.splitext(img_name)
             if ext == "bpx" or ext == "BPX":
                 continue # Skip database.bpx file
+            print(f'Processing {img_name} ...')
             input_img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+
+            if input_img is None: #Another attempt at getting python to accept skipping BPX files
+                continue
 
             cropped_faces, restored_faces, restored_img = restorer.enhance(
                 input_img, has_aligned=args.aligned, only_center_face=args.only_center_face, paste_back=args.paste_back)
